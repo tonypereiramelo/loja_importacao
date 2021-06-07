d33f00ac2304925730c1a5c9896a33f58af03244
tonypereiramelo <tonypereiramelo@gmail.com>
2021-06-07 10:59:24 -0300

    card pedidos

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OrderTile extends StatelessWidget {
@@ -7,7 +8,32 @@ class OrderTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text(orderId!),
+      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: StreamBuilder<DocumentSnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("orders")
+              .doc(orderId)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            else {
+              return Column(
+                children: <Widget>[
+                  Text(
+                    "Código do pedido: ${snapshot.data!.id}",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
